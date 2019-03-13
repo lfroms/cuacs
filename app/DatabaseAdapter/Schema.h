408a52a406b0ca2226d42e483558e25288080369
Lukas Romsicki <lukas@romsicki.com>
2019-03-13 12:43:30 -0400

    Clean up schema

--- app/DatabaseAdapter/Schema.h
+++ app/DatabaseAdapter/Schema.h
@@ -9,42 +9,40 @@ static const QString ATTRIBUTE_TABLE = "attributes";
 static const QString ANIMAL_ATTRIBUTE_TABLE = "animals_attributes";
 
 static const QString ANIMAL_SCHEMA =
-        "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
-        "species TEXT NOT NULL,"
-        "gender TEXT NOT NULL,"
-        "main_color TEXT NOT NULL,"
-        "breed TEXT NOT NULL,"
-        "age INTEGER NOT NULL,"
-        "neutered_or_spayed INTEGER NOT NULL,"
-        "requires_medical INTEGER NOT NULL,"
-        "name TEXT NOT NULL,"
-        "hypothetical INTEGER NOT NULL,"
-        "bite_tendency INTEGER NOT NULL,"
-        "scratch_tendency INTEGER NOT NULL,"
-        "dominance_tendency INTEGER NOT NULL,"
-        "friendliness_adults INTEGER NOT NULL,"
-        "friendliness_children INTEGER NOT NULL,"
-        "friendliness_animals INTEGER NOT NULL,"
-        "noise_level INTEGER NOT NULL,"
-        "independence INTEGER NOT NULL,"
-        "affection INTEGER NOT NULL,"
-        "energy_level INTEGER NOT NULL,"
-        "anxiety_level INTEGER NOT NULL,"
-        "curiosity_level INTEGER NOT NULL";
+        QString(
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+            "name TEXT NOT NULL,"
+            "species TEXT NOT NULL,"
+            "gender TEXT NOT NULL,"
+            "breed TEXT NOT NULL,"
+            "age INTEGER NOT NULL,"
+            "neutered_or_spayed INTEGER NOT NULL,"
+            "requires_medical INTEGER NOT NULL,"
+            "main_color TEXT NOT NULL,"
+            "hypothetical INTEGER NOT NULL"
+            );
 
 static const QString CLIENT_SCHEMA =
-        "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
-        "name TEXT NOT NULL,"
-        "age INTEGER NOT NULL,"
-        "phone_number TEXT NOT NULL,"
-        "email TEXT NOT NULL";
+        QString(
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+            "name TEXT NOT NULL,"
+            "age INTEGER NOT NULL,"
+            "phone_number TEXT NOT NULL,"
+            "email TEXT NOT NULL,"
+            "ideal_animal_id INTEGER,"
+            "FOREIGN KEY (ideal_animal_id) REFERENCES %1(id)"
+            )
+        .arg(ANIMAL_TABLE);
 
 static const QString ATTRIBUTE_SCHEMA =
-        "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
-        "name TEXT NOT NULL,"
-        "description TEXT";
+        QString(
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+            "name TEXT NOT NULL,"
+            "description TEXT"
+            );
 
-static const QString ANIMAL_ATTRIBUTE_SCHEMA = QString(
+static const QString ANIMAL_ATTRIBUTE_SCHEMA =
+        QString(
             "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
             "animal_id INTEGER NOT NULL,"
             "attribute_id INTEGER NOT NULL,"
