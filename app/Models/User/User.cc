9b78ec15bd22a423f81da06ebddb26a02a8cda19
Lukas Romsicki <lukas@romsicki.com>
2019-03-19 00:25:18 -0400

    Add implementation for user model

--- app/Models/User/User.cc
+++ app/Models/User/User.cc
@@ -1,5 +1,28 @@
 #include "User.h"
 
-User::User() {
+User::User(
+        QString username,
+        QString password
+        ) {
+    this->username = username;
+    this->password = password;
+}
 
+User::~User() {}
+
+// Define how the SQL record should be decomposed into this object.
+User::User(QSqlRecord* record) :
+    User(record->field("username").value().toString(),
+         record->field("password").value().toString()) {}
+
+const QString User::toCommaSeparated() {
+    QString formatted = QString("'%1', '%2'")
+            .arg(username)
+            .arg(password);
+
+    return formatted;
+}
+
+const QString User::className() {
+    return "user";
 }
--- app/Models/User/User.h
+++ app/Models/User/User.h
@@ -1,9 +1,24 @@
 #ifndef USER_H
 #define USER_H
 
-class User {
+#include <QString>
+#include <ActiveObject/ActiveObject.h>
+
+class User : public ActiveObject<User> {
 public:
-    User();
+    User(QString username, QString password);
+    User(QSqlRecord* record);
+    ~User();
+
+    const QString toCommaSeparated();
+    static const QString className();
+
+protected:
+    QString getUsername();
+
+private:
+    QString username;
+    QString password;
 };
 
 #endif // USER_H
