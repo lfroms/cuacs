9415c33845863fa09c1d0bc2c887cd3ce1a790fd
Lukas Romsicki <lukas@romsicki.com>
2019-03-19 23:07:22 -0400

    Add basic functionality to LoginWindow

--- app/LoginWindow/LoginWindow.cc
+++ app/LoginWindow/LoginWindow.cc
@@ -3,12 +3,38 @@
 
 LoginWindow::LoginWindow(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::LoginWindow)
-{
+    ui(new Ui::LoginWindow) {
+
     ui->setupUi(this);
 }
 
-LoginWindow::~LoginWindow()
-{
+LoginWindow::~LoginWindow() {
     delete ui;
+}
+
+void LoginWindow::handleLogInAction() {
+    QMessageBox messageBox;
+    messageBox.setWindowTitle("Login Error");
+    messageBox.setText("Incorrect username or password.");
+
+
+    QString enteredUsername = ui->username->text();
+    QString enteredPassword = ui->password->text();
+
+    QVector<User*>* usernameMatches = User::where("username", enteredUsername);
+
+    if (usernameMatches->isEmpty()) {
+        messageBox.exec();
+        return;
+    }
+
+    QVector<User*>* passwordMatches = User::where("password", enteredUsername);
+
+    if (passwordMatches->isEmpty()) {
+        messageBox.exec();
+        return;
+    }
+
+    CurrentUser::user = usernameMatches->first();
+    this->close();
 }
--- app/LoginWindow/LoginWindow.h
+++ app/LoginWindow/LoginWindow.h
@@ -2,18 +2,25 @@
 #define LOGINWINDOW_H
 
 #include <QDialog>
+#include <QString>
+#include <QVector>
+#include <QMessageBox>
+#include <Models/User/User.h>
+#include <Models/CurrentUser/CurrentUser.h>
 
 namespace Ui {
 class LoginWindow;
 }
 
-class LoginWindow : public QDialog
-{
+class LoginWindow : public QDialog {
     Q_OBJECT
 
 public:
     explicit LoginWindow(QWidget *parent = nullptr);
     ~LoginWindow();
+
+private slots:
+    void handleLogInAction();
 
 private:
     Ui::LoginWindow *ui;
